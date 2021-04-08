6098b463a4175f242d9cb08fbf209aedf9d21152
Rafael Silvestri <rafael.silvestri@thomsonreuters.com>
2021-04-07 22:41:57 -0300

    updates

---
+++ cloud-formation/spotfleet/test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import base64
+
+print("hello")
+
+script = b'''
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
+'''
+
+encoded = base64.b64encode(script).decode("ascii")
+
+print(encoded)
+print("-------")
+print(base64.b64decode(encoded))
+
